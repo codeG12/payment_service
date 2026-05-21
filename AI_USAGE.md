4f4d27704ac830520fc26dda71bd457c2638e82e
codeG12 <gunasridhar122001@gmail.com>
2026-05-21 23:43:08 +0530

    initial code has been compelted

---
+++ AI_USAGE.md
@@ -0,0 +1,3 @@
+# AI Usage Log
+
+I have used Gemini to understand the business requirements and to understand the terms like PSP(I know payment providers like stripe this term is new to me)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tracing::{Level, info};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    info!("handling request");
+    "Hello, world!"
 }
